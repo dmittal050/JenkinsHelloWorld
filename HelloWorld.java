2c64a6c7d680cd40bd51ca71ba3e9334b7731f87
dmittal050 <dmittal050@gmail.com>
2025-09-28 16:33:11 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
  public class HelloWorld{
 
     public static void main(String[] args){
-	System.out.println("I am learning jenkins with GIT 1.....");
+	System.out.println("I am learning jenkins with GIT with webhooks.....");
     }
 
 }
