a281a38058b07fb09247483d10a3b928496984bc
dmittal050 <dmittal050@gmail.com>
2025-09-28 16:06:44 +0530

    third commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
  public class HelloWorld{
 
     public static void main(String[] args){
-	System.out.println("I am learning jenkins....");
+	System.out.println("I am learning jenkins with GIT.....");
     }
 
 }
